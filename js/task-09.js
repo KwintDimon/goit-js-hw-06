448062e1167fb1435e748bbbc01d51e06a817e88
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-05-20 11:47:05 +0300

    finish task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const refs={
+  body: document.querySelector('body'),
+  btn: document.querySelector('.change-color'),
+  span:document.querySelector('.color'),
+}
+
+refs.btn.addEventListener('click', ()=> {
+  const color = getRandomHexColor();
+  refs.span.textContent = color;
+   refs.body.style.backgroundColor = color;
+})
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
